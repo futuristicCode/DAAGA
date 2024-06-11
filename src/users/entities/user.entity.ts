51c0316ec124b967363e26d7c68f2013b9534653
futuristicCode <baoumaramadoubocar@gmail.com>
2024-06-11 03:02:57 +0000

    Create User Entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,45 @@
-export class User {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+    lastName: string;
+    
+  @Column()
+  gender: string;
+  
+  @Column()
+  address: string;
+  
+  @Column()
+  phoneNumber: string;
+
+  @Column()
+  email: string;
+    
+  @Column()
+  accountNumber: string;
+  
+  @Column()
+  accountBalance: number;
+    
+  @Column()
+  status: string;
+
+  @Column({ default: true })
+    isActive: boolean;
+    
+ @CreateDateColumn()
+    createdAt: Date
+
+ @UpdateDateColumn()
+    updateAt: Date
+    
+
+}
